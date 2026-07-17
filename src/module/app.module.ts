701333beb271392846be1b27c5bde5b08dffb02c
vinicius Campbell dos Santos <vinicius.campbell.etm2018.2@gmail.com>
2026-07-17 20:20:32 -0300

    tentativa login 1

--- src/module/app.module.ts
+++ src/module/app.module.ts
@@ -3,10 +3,13 @@ import { Module } from '@nestjs/common';
 import { EpiModule } from './epi.module';
 import { RiscoModule } from './risco.module';
 import { TreinamentoModule } from './treinamento.module';
+import { AuthService } from '../service/auth/auth.service';
+import { AuthController } from '../controller/auth/auth.controller';
+
 
 @Module({
   imports: [EpiModule, RiscoModule, TreinamentoModule],
-  controllers: [],
-  providers: [],
+  controllers: [AuthController],
+  providers: [AuthService],
 })
 export class AppModule {}
